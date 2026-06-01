3e1d5935dc44c5f6cbabc38b8f85677bb7088e31
wenxu <wenxu@wenxudeMacBook-Pro.local>
2026-06-01 11:29:46 +0800

    feat: configure server port from args

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,25 +1,34 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"personal-manager/internal/handler"
 	"personal-manager/internal/service"
 	"personal-manager/internal/store"
 )
 
+const defaultPort = 8080
+
 func main() {
+	port, err := parsePort(os.Args[1:], os.Stderr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	dbPath := os.Getenv("PERSONAL_MANAGER_DB")
 	if dbPath == "" {
 		dbPath = "personal_manager.db"
 	}
 
-	addr := os.Getenv("ADDR")
-	if addr == "" {
-		addr = ":8080"
-	}
+	addr := fmt.Sprintf(":%d", port)
 
 	st, err := store.Open(dbPath)
 	if err != nil {
@@ -34,4 +43,44 @@ func main() {
 	if err := http.ListenAndServe(addr, router); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func parsePort(args []string, output io.Writer) (int, error) {
+	fs := flag.NewFlagSet("server", flag.ContinueOnError)
+	fs.SetOutput(output)
+
+	port := fs.Int("port", defaultPort, "external service port")
+	if err := fs.Parse(args); err != nil {
+		return 0, err
+	}
+
+	portSet := false
+	fs.Visit(func(f *flag.Flag) {
+		if f.Name == "port" {
+			portSet = true
+		}
+	})
+
+	switch fs.NArg() {
+	case 0:
+		return validatePort(*port)
+	case 1:
+		if portSet {
+			return 0, fmt.Errorf("port must be provided either as -port or as an argument, not both")
+		}
+		positionalPort, err := strconv.Atoi(fs.Arg(0))
+		if err != nil {
+			return 0, fmt.Errorf("port must be a number")
+		}
+		return validatePort(positionalPort)
+	default:
+		return 0, fmt.Errorf("unexpected arguments: %s", strings.Join(fs.Args(), " "))
+	}
+}
+
+func validatePort(port int) (int, error) {
+	if port < 1 || port > 65535 {
+		return 0, fmt.Errorf("port must be between 1 and 65535")
+	}
+	return port, nil
 }
